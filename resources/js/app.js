4131c3edf56b5a701e1464e223de876947cd63ad
João M <dev.joaomarcoslima@gmail.com>
2023-06-23 20:59:38 -0300

    removed old components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,4 @@
 import Alpine from 'alpinejs'
-import copyableInput from './components/copyable-input'
-import themeToggler from './components/theme-toggler'
 
 import axios from 'axios';
 window.axios = axios;
@@ -9,8 +7,5 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Alpine = Alpine
 
 Alpine.magic('axios', () => axios)
-
-Alpine.data('copyableInput', copyableInput);
-Alpine.data('themeToggler', themeToggler);
 
 Alpine.start()

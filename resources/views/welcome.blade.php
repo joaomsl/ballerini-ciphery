d39baee805f04e1a76436d99f530b052ccf2cceb
João M <dev.joaomarcoslima@gmail.com>
2023-06-23 20:59:21 -0300

    changed name of ciphery component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <x-layout class="grid place-items-center min-h-screen" page-title="Ballerini Ciphery">
     <div class="container p-3 md:p-0">
         <x-header />
-        <x-ciphery-form />
+        <x-ciphery />
     </div>
 </x-layout>

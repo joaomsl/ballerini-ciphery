f801cccf05eb17b865fbd6d6fcc69d3372fb1d9e
João M <dev.joaomarcoslima@gmail.com>
2023-06-25 00:04:58 -0300

    installed alpine persist plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,12 +3,14 @@ import Alpine from 'alpinejs'
 import axios from 'axios';
 import ciphery from './components/ciphery';
 import badge from './components/badge';
+import persist from '@alpinejs/persist'
 
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 window.Alpine = Alpine
 
+Alpine.plugin(persist)
 Alpine.magic('axios', () => axios)
 
 Alpine.data('ciphery', ciphery)

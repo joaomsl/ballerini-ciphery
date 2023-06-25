8a321be403cc4f6a617d5283c2ebd528edda17ab
João M <dev.joaomarcoslima@gmail.com>
2023-06-24 21:20:12 -0300

    pre-processing collection values

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Ciphery\Characteristics\CharacteristicsCollection;
 use App\Ciphery\HashingAlgo;
 use App\Ciphery\Characteristics;
+use App\Ciphery\Contracts\Option;
 use App\Ciphery\HashingAlgo\HashingAlgoCollection;
 use Closure;
 use Illuminate\Support\ServiceProvider;
@@ -21,25 +22,36 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton(CharacteristicsCollection::class, Closure::fromCallable([$this, 'makeCharacteristicsCollection']));
     }
 
+    /**
+     * @param Option[] $options
+     * @return array<string, Option>
+     */
+    protected function optionIdHasKeys(array $options): array
+    {
+        return collect($options)
+            ->keyBy(fn(Option $option) => $option->getId())
+            ->all();
+    }
+
     /** @return HashingAlgoCollection */
     protected function makeHashingAlgoCollection(): HashingAlgoCollection
     {
-        return new HashingAlgoCollection([
+        return new HashingAlgoCollection($this->optionIdHasKeys([
             new HashingAlgo\Md5HashingAlgo('md5', 'MD5'),
             new HashingAlgo\Sha1HashingAlgo('sha1', 'SHA-1'),
             new HashingAlgo\BcryptHashingAlgo('bcrypt', 'Bcrypt')
-        ]);
+        ]));
     }
 
     /** @return CharacteristicsCollection */
     protected function makeCharacteristicsCollection(): CharacteristicsCollection
     {
-        return new CharacteristicsCollection([
+        return new CharacteristicsCollection($this->optionIdHasKeys([
             new Characteristics\CapitalLettersCharacteristic('capital_letters', 'ABC'),
             new Characteristics\SmallLettersCharacteristic('small_letters', 'abc'),
             new Characteristics\NumbersCharacteristic('numbers', '123'),
             new Characteristics\SymbolsCharacteristic('symbols', '!#@')
-        ]);
+        ]));
     }
 
 }

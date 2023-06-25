0ea8d3620b8c82f52ff950bbb897c5f13c419dbd
João M <dev.joaomarcoslima@gmail.com>
2023-06-25 13:38:18 -0300

    dark mode fixes

--- resources/views/components/load-ring.blade.php
+++ resources/views/components/load-ring.blade.php
@@ -1,8 +1,8 @@
 @props(['variant' => 'primary'])
 
 @php($classesByVariant = match($variant) {
-    'secondary' => 'border-t-gray-500 border-gray-400/70',
+    'secondary' => 'border-t-gray-500 dark:border-t-gray-300 border-gray-400/70',
     default => 'border-rose-400 border-t-gray-100'
 })
 
-<span {{ $attributes->class([$classesByVariant, 'animate-spin w-5 h-full leading-none aspect-square border-4 rounded-full']) }}></span>
+<span {{ $attributes->class([$classesByVariant, 'animate-spin w-5 h-5 leading-none aspect-square border-4 rounded-full']) }}></span>
